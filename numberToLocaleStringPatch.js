dc9c1a4851d9a9eb949c5d84b53912a612857b48
Tyler Stark <theirondeveloper@gmail.com>
2013-07-29 08:48:17 -0500

    Following helpful advice from Zomgondo on reddit
    http://www.reddit.com/r/javascript/comments/1j9gev/requesting_suggestions_to_improve_a_very_small_js/cbchmwf

--- numberToLocaleStringPatch.js
+++ numberToLocaleStringPatch.js
@@ -1,8 +1,8 @@
 (function(){
 	var tempNumber = 1000;
-	// toLocaleString *should* return different strings based on different locales
-	if(	tempNumber.toLocaleString('en') !== '1,000' ||
-		tempNumber.toLocaleString('fr') !== '1 000') {
+	//  German and French localized numbers do not equal eachother.
+	// If the following passes, it suggests that the locale parameter is being ignored.
+	if(tempNumber.toLocaleString('de') === tempNumber.toLocaleString('fr') ) {
 			window.Number.prototype.toLocaleString = function(){
 				var currentNumber = this.toString(),
 					locale,
@@ -11,7 +11,7 @@
 				navigatorLanguage = (navigator && navigator.language) || 'en';									
 				locale = arguments[0] || navigatorLanguage;
 				
-				// TODO: Find more brands
+				// TODO: Find more locales
 				// TODO: Intelligently arrange formatting.
 				/** 	For instance, DE and AT will likely format numbers the same.
 				 *		Rather than have non-Dry code, we can categorize formatting based on type.

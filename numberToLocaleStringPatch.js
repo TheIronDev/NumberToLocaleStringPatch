068d9e71341188e170760dee8b9ba1acf1997be0
TheIronDeveloper <theirondeveloper@gmail.com>
2013-07-30 02:12:10 -0500

    Following Zomgondo's suggestions, I went ahead and used Math.PI*100000 to generate a number. I also cleaned up the code, now all formatting takes place in the helper function, formatNumber. I updated the tests to factor in decimal numbers

--- numberToLocaleStringPatch.js
+++ numberToLocaleStringPatch.js
@@ -1,31 +1,43 @@
+/* Author: Tyler Stark 
+ * Name: Localized Numbers Patch
+ * Purpose: Patch Legacy Browsers Usage of toLocaleString
+ **/
 (function(){
-	var tempNumber = 1000;
+	var tempNumber = Math.PI*1000000;
 	//  German and French localized numbers do not equal eachother.
 	// If the following passes, it suggests that the locale parameter is being ignored.
 	if(tempNumber.toLocaleString('de') === tempNumber.toLocaleString('fr') ) {
+			// Helper Function to assist with formatting numbers
+			var formatNumber = function(number, delimiter, decimalDelimiter){
+				var numberString = number.toString(), 
+					splitNumber = numberString.split('.'),
+					splitFloats = '';
+
+				// Check if the number's precision is greater than the thousanths place.
+				if(splitNumber[1] && splitNumber[1].length > 3) {
+					// If so, build out the tail end of the formatted Number.
+					splitFloats = decimalDelimiter + number.toFixed(3).toString().split('.')[1];
+				}
+
+				return splitNumber[0].split( /(?=(?:\d{3})+(?:\.|$))/g ).join(delimiter) + splitFloats;
+			};
 			window.Number.prototype.toLocaleString = function(){
-				var currentNumber = this.toString(),
-					locale,
-					navigatorLanguage;
+				var locale,
+					navigatorLanguage,
+					numberPrecision;
+				
 
 				navigatorLanguage = (navigator && navigator.language) || 'en';									
 				locale = arguments[0] || navigatorLanguage;
 				
-				// TODO: Find more locales
-				// TODO: Intelligently arrange formatting.
-				/** 	For instance, DE and AT will likely format numbers the same.
-				 *		Rather than have non-Dry code, we can categorize formatting based on type.
-				**/
+				// TODO: Find more locales				
 				switch(locale) {
 					case 'en':
-						return currentNumber.split( /(?=(?:\d{3})+(?:\.|$))/g ).join( "," );
-						break;
+						return formatNumber(this, ',', '.');
 					case 'fr':
-						return currentNumber.split( /(?=(?:\d{3})+(?:\.|$))/g ).join( " " );
-						break;
+						return formatNumber(this, ' ', ',');
 					case 'de':
-						return currentNumber.split( /(?=(?:\d{3})+(?:\.|$))/g ).join( "." );
-						break;
+						return formatNumber(this, '.', ',');
 				}
 
 				// Failsafe scenerio
--- tests/numberToLocaleStringSpec.js
+++ tests/numberToLocaleStringSpec.js
@@ -6,18 +6,18 @@ describe("Number", function(){
 	var number;
 
 	beforeEach(function() {
-		number = 1000;
+		number = Math.PI*1000000;
 		// TODO: Add an override in the event that phantomJS does properly handle en, fr, and de.
 	});
 
-	it("should produce '1,000' for English Locales", function() {		
-		expect(number.toLocaleString('en')).toEqual("1,000");
+	it("should produce '3,141,592.654' for English Locales", function() {		
+		expect(number.toLocaleString('en')).toEqual("3,141,592.654");
 	});
-	it("should produce '1 000' for French Locales", function() {		
-		expect(number.toLocaleString('fr')).toEqual("1 000");
+	it("should produce '3 141 592,654' for French Locales", function() {		
+		expect(number.toLocaleString('fr')).toEqual("3 141 592,654");
 	});
-	it("should produce '1.000' for German Locales", function() {		
-		expect(number.toLocaleString('de')).toEqual("1.000");
+	it("should produce '3.141.592,654' for German Locales", function() {		
+		expect(number.toLocaleString('de')).toEqual("3.141.592,654");
 	});
 	
 });
